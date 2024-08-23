c4d4c5457f0e4df70e6514ec7d2abc5c52cfcb6f
Kien Pham <kienpham@amazon.com>
2024-08-23 15:06:12 -0700

    clean up

--- src/tooluse.py
+++ src/tooluse.py
@@ -1,5 +1,5 @@
-# from dotenv import load_dotenv
-# load_dotenv()
+from dotenv import load_dotenv
+load_dotenv()
 import boto3
 import os
 import json
@@ -23,29 +23,13 @@ def provider_websearch(query):
     print(f"implement websearch here...: {query}")
     # TODO: advance search, sort by last Edited?
     pages = wikipedia.search(query)
-    # print(pages)
-    # exit()
 
     # TODO: assume the 1st page is the best. Rerank?
     page1 = wikipedia.page(pages[0])
     page2 = wikipedia.page(pages[1])
-    # page3 = wikipedia.page(pages[2])
     
-    # print(res.url)
     payload = "{} \n {} \n".format(page1, page2)
     return payload 
-
-    # return res.content
-
-    # payload = {
-    #     "title": res.title,
-    #     "url": res.url,
-    #     "content": res.content
-    # }
-    # return payload 
-
-def provider_catch_all(query):
-    print(f"Catch-all / fallback provider: {query}")
 
 provider_websearch_schema = {
     "toolSpec": {
@@ -63,34 +47,11 @@ provider_websearch_schema = {
     }
 }
 
-provider_catch_all_schema = {
-    "toolSpec": {
-        "name": "provider_catch_all",
-        "description": "A tool to handle any generic query that other previous tools can't answer.",
-        "inputSchema": {
-            "json": {
-            "type": "object",
-            "properties": {
-                "query": {"type": "string", "description": "The user query that other previous tool doesn't understand."}
-            },
-            "required": ["query"]
-            }
-        }
-    }
-}
-
 toolConfig = {
     "tools": [provider_websearch_schema],
-    # "toolChoice": {
-    #     "any":{},    # must trigger one of the available tools
-    #     # "auto":{}, # default
-    #     # "tool":{   # always trigger this tool
-    #     #     "name": "provider_websearch"
-    #     # },
-    # }
 }
 
-def guardrails(prompt): 
+def guardrails(prompt):
     response = bedrock_client.apply_guardrail(
         guardrailIdentifier=os.environ.get('GUARDRAILS_ID'), # ex: '453cg26ykbxy'
         guardrailVersion='1',
@@ -106,25 +67,23 @@ def guardrails(prompt):
             },
         ]
     )
-    # print(response)
-    return response 
+    return response
     
 def router(user_query, enable_guardrails=False):
     # apply guardrails 
     if enable_guardrails:
         guard = guardrails(user_query)
         if guard['action'] == 'GUARDRAIL_INTERVENED':
-            print("Guardrails blocked this action", guard["assessments"])
+            print("Guardrails blocked this action: ", guard["assessments"])
             return
 
     messages = [{"role": "user", "content": [{"text": user_query}]}]
 
     system_prompt=f"""
-          You will be asked a question by the user. 
+    You will be asked a question by the user. 
     If answering the question requires data you were not trained on, you must use the get_article tool to get the contents of a recent wikipedia article about the topic. 
     If you can answer the question without needing to get more information, please do so. 
     Only call the tool when needed. 
-
     """
 
     converse_api_params = {
@@ -181,13 +140,8 @@ def router(user_query, enable_guardrails=False):
           print("Claude's final answer:")
           print(final_res['output']['message']['content'][0]['text'])
 
-        # print(response['usage'])
-        # print(response['metrics'])
-        
-
 if __name__ == "__main__":
     # router("Why is the sky blue?")
     # router("Which countries have the most medal in the Olympic 2024?")
     # router("Who's the current President of the United States?")
     router("Who are the candidate running for US President in 2024?")
-    
